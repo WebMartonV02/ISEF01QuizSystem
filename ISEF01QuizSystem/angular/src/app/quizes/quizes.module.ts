c7e0aac88fafbff4246ab3b927db8dc032ee2a78
marto <martonvprog@gmail.com>
2024-05-05 18:44:32 +0200

    designs and html files are prepared

--- ISEF01QuizSystem/angular/src/app/quizes/quizes.module.ts
+++ ISEF01QuizSystem/angular/src/app/quizes/quizes.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from '../shared/shared.module';
 import { QuizesRoutingModule } from './quizes-routing.module';
 import { QuizesComponent} from './quizes.component';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [QuizesComponent],
-  imports: [SharedModule, QuizesRoutingModule, MatBadgeModule],
+  imports: [SharedModule, QuizesRoutingModule, MatBadgeModule, MatIconModule],
 })
 export class QuizesModule {}
