e5f8c9180147feed6d8a4358167f485215fd3097
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2024-05-25 12:47:26 +0200

    updated quizhub

--- ISEF01QuizSystem/angular/src/app/quizhub/quizhub.component.ts
+++ ISEF01QuizSystem/angular/src/app/quizhub/quizhub.component.ts
@@ -1,5 +1,11 @@
 import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Comments } from '@proxy';
+import { CommentService } from '@proxy/comments';
+import { CourseResponseDto, CourseService } from '@proxy/courses';
+import { Subject, takeUntil } from 'rxjs';
+
 
 @Component({
   selector: 'app-quizhub',
@@ -7,13 +13,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./quizhub.component.scss'],
 })
 export class QuizhubComponent {
-  get hasLoggedIn(): boolean {
-    return this.authService.isAuthenticated;
+  public entities:  Array<CourseResponseDto>;
+  selectedValue: string;
+
+  questionTitle: string;
+  questionComment: string;
+
+  private _componentDestroyed$: Subject<void> = new Subject();
+
+  constructor(
+    private readonly _courseService: CourseService,
+    private readonly _commentsService: CommentService, 
+    private readonly _router: Router) {}
+
+  public ngOnInit(): void
+  {
+    this.LoadCourses();
   }
 
-  constructor(private authService: AuthService) {}
+  public ngOnDestroy(): void
+  {
+    this._componentDestroyed$.next();
+    this._componentDestroyed$.complete();
+  }
 
-  login() {
-    this.authService.navigateToLogin();
+  public LoadCourses() : void
+  {
+    this._courseService.getAll()
+      .pipe(takeUntil(this._componentDestroyed$))
+      .subscribe((data: Array<CourseResponseDto>) =>
+      {
+        this.entities = data;
+      })
+  }
+
+  public RouteToQuiz(courseId: number) :  void
+  {
+    let _ = this._router.navigate(['/quizes', courseId]);
+  }
+
+  public publish() {
+    
   }
 }
