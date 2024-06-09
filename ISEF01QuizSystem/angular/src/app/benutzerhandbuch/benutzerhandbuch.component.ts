98d24830d91dac3e5e7619d11942f7ce625d49ff
marto <martonvprog@gmail.com>
2024-06-09 15:27:15 +0200

    added user manual link

--- ISEF01QuizSystem/angular/src/app/benutzerhandbuch/benutzerhandbuch.component.ts
+++ ISEF01QuizSystem/angular/src/app/benutzerhandbuch/benutzerhandbuch.component.ts
@@ -4,6 +4,7 @@ import { CommentResultDto, CommentService } from '@proxy/comments';
 import { Subject, takeUntil } from 'rxjs';
 import { Router } from '@angular/router';
 import { ConfigStateService } from '@abp/ng.core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-benutzerhandbuch',
@@ -17,8 +18,6 @@ export class BenutzerhandbuchComponent
   selectedValue: string;
   currentCourseId: number;
 
-  questionTitle: string;
-  questionComment: string;
   currentUserId: any;
 
   private _componentDestroyed$: Subject<void> = new Subject();
@@ -27,7 +26,8 @@ export class BenutzerhandbuchComponent
     private readonly _courseService: CourseService,
     private readonly _commentsService: CommentService,
     private readonly _router: Router,
-    private config: ConfigStateService) {}
+    private config: ConfigStateService,
+    private readonly _httpClient: HttpClient) {}
 
   public ngOnInit(): void
   {
@@ -48,62 +48,6 @@ export class BenutzerhandbuchComponent
       .subscribe((data: Array<CourseResponseDto>) =>
       {
         this.entities = data;
-      })
-  }
-
-  public LoadComments(): void
-  {
-    this._commentsService.getCommentsOrderedForCourseByCourseId(this.currentCourseId)
-      .pipe(takeUntil(this._componentDestroyed$))
-      .subscribe((data: Array<CommentResultDto>) =>
-      {
-        console.log(data);
-        this.comments = data;
-      })
-  }
-
-  public RouteToQuiz(courseId: number) :  void
-  {
-    let _ = this._router.navigate(['/quizes', courseId]);
-  }
-
-  public publish() {
-    const currentUserId = this.config.getOne("currentUser").id;
-
-    var result = this._commentsService.createCommentForCourseByRequestDto({
-      id: this.comments.length+2, courseId: this.currentCourseId, content: this.questionComment, userId: currentUserId
-    }).
-    pipe(takeUntil(this._componentDestroyed$))
-      .subscribe((response) =>
-      {
-        this.LoadComments();
-        this.questionComment = '';
-        console.log(response)
-      })
-  }
-
-  public courseChanged(value: string) {
-    this.currentCourseId = this.getIdFromCourse(value);
-    this.LoadComments();
-  }
-
-  private getIdFromCourse(name: string) : number{
-    for(var i = 0; i< this.entities.length; i++) {
-      if(this.entities[i].title === this.selectedValue){
-        return this.entities[i].id;
-      }
-    }
-    return 0;
-  }
-
-  public deleteComment(commentId: number) {
-    var result = this._commentsService.deleteCommentByCommentId(commentId).
-    pipe(takeUntil(this._componentDestroyed$))
-      .subscribe((response) =>
-      {
-        this.LoadComments();
-        this.questionComment = '';
-        console.log(response)
       })
   }
 }
